3f9fb9185e1b16aae9c4c786c886173b2362d291
小烜同学 <1091598365@qq.com>
2019-07-29 10:34:25 +0800

    docs: lc 2

--- leetcode/2.两数相加.js
+++ leetcode/2.两数相加.js
@@ -39,4 +39,35 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {};
+var addTwoNumbers = function(l1, l2) {
+  function ListNode(val) {
+    this.val = val;
+    this.next = null;
+  }
+  // 哨兵位
+  const newLink = new ListNode(-1);
+  let cur = newLink;
+  // 用于判断是否需要进位
+  let carry = 0;
+  while (l1 || l2) {
+    const val1 = (l1 && l1.val) || 0;
+    const val2 = (l2 && l2.val) || 0;
+    let sum = val1 + val2 + carry;
+
+    carry = (sum / 10) | 0;
+    sum = sum % 10;
+    cur.next = new ListNode(sum);
+
+    cur = cur.next;
+    if (l1) {
+      l1 = l1.next;
+    }
+    if (l2) {
+      l2 = l2.next;
+    }
+  }
+  if (carry === 1) {
+    cur.next = new ListNode(carry);
+  }
+  return newLink.next;
+};
